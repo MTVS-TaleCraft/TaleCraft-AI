63aaa82deb21eb5c9e49582344df876e3096418b
smg0218 <smg000218@gmail.com>
2025-07-23 14:33:44 +0900

    models.py에 결과정보 추가 chatbot.py에 이름 추천과 분량 늘리기 추가

--- models.py
+++ models.py
@@ -3,10 +3,16 @@ from typing import Optional, List, Dict, Any
 
 # 요청 모델 정의
 class ChatRequest(BaseModel):
-    query: str = Field(..., description="사용자 질문")
+    question: str = Field(..., description="사용자 질문")
+    image: Optional[str] = Field(default=None, description="이미지 정보")
     beforeQuestionList: Optional[List[str]] = Field(
         default=None, description="이전 질문 목록"
     )
     beforeResponseList: Optional[List[str]] = Field(
         default=None, description="이전 대답 목록"
     )
+    option: Optional[int] = Field(default=None, description="늘려야 될 길이")
+
+class ChatResponse(BaseModel):
+    success: bool = Field(..., description= "성공 여부")
+    answer: str = Field(..., description= "답변 내용")
